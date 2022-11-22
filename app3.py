9ba12717f685290fc913da08e692e266174399a1
Jason Carvalho <jase@jasoncarvalho.com>
2022-11-22 12:49:24 +0000

    Updated Activity Log pull script

--- app3.py
+++ app3.py
@@ -14,11 +14,11 @@ nlp = spacy.load("en_core_web_sm")
 
 
 def funcCheckUserRequest():
-    url = "xxxxx"
+    url = 'https://api2.pp.mksmart.org/browse/spice__activity_log'
     headers = {
-        'Authorization': 'xxxxxx'
+        'Authorization': 'Basic YWN0aXZpdHlsb2dfcmVhZGtleTphY3Rpdml0eWxvZ19yZWFka2V5'
     }
-    response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", url, headers=headers, verify=False)
     if response.status_code == 200:
 
         data = response.json()
@@ -37,11 +37,13 @@ def funcCheckUserRequest():
 
 def runUserRequest():
     print("****Welcome to my bot*****")
-    namesResult = funcCheckUserRequest()
-    api_names = namesResult['name']
+    # namesResult = funcCheckUserRequest()
+    # api_names = namesResult['name']
     character_names = []
     with open("data2.txt", "r", encoding="utf-8") as f:
-        characters = f.read().split("\n\n")
+        characters = f.read().split("\t")
+        print("***characters***")
+        print(characters)
         # characters = api_names
 
         for character in characters:
@@ -51,29 +53,49 @@ def runUserRequest():
                 chedck_value = CommonRegex(name)
                 if chedck_value.times:
                     character_names.append(chedck_value.times)
+                    print("found times")
                 if chedck_value.dates:
                     character_names.append(chedck_value.dates)
+                    print("found dates")
                 if chedck_value.links:
                     character_names.append(chedck_value.links)
+                    print("found links")
                 if chedck_value.phones:
                     character_names.append(chedck_value.phones)
+                    print("found phones")
                 if chedck_value.emails:
                     character_names.append(chedck_value.emails)
+                    print("found emails")
                 if chedck_value.credit_cards:
                     character_names.append(chedck_value.credit_cards)
+                    print("found cards")
                 if chedck_value.street_addresses:
                     character_names.append(chedck_value.street_addresses)
+                    print("found addresses")
                 if chedck_value.ips:
                     character_names.append(chedck_value.ips)
+                    print("found ips")
+
+        print("*** printing character names ***")
+        print(character_names)
 
         charactersNames = re.sub('[!@#$]', '', str(characters))
         test = charactersNames
         nlp = spacy.load("en_core_web_sm")
         doc = nlp(test)
         for ent in doc.ents:
+            print("entity")
+            print(ent)
             name = ent.text, ent.label_
             character_names.append(name)
 
+        print("*** printing character names after NLP ***")
+        print(character_names)
+
+    # If length of character_names array > 0 (ie we found something), then build an appropriate JSON response, else return false
+
+    return True
+'''
     return {
         "job-type": "PRIVACY-VIOLATION",
         "submitted-by": "privacy-module",
@@ -88,13 +110,18 @@ def runUserRequest():
         "dataset": "",
         "status": "ALERT"
     }
+'''
 
 
 def printVal():
     print(runUserRequest())
 
 
+print(runUserRequest())
+
+'''
 schedule.every(1).minutes.do(printVal)
 while 1:
     schedule.run_pending()
     time.sleep(1)
+'''
--- pullFromActivityLog.py
+++ pullFromActivityLog.py
@@ -3,22 +3,23 @@ from requests.auth import HTTPBasicAuth
 import json
 
 baseUrl = 'https://api2.pp.mksmart.org/browse/'
-alDataset = '<activity_log_dataset_here>'
-alDatasetKey = '<key_here>'
-pagesize = 100
+alDataset = 'spice__activity_log'
+alDatasetKey = 'activitylog_readkey'
+pagesize = 20
 page = 1
+initialTimestamp = str(1667306718)   # 1st November 2022
 
 # Exclude these datasets from our analysis
 # At the very least, this should include the dataset we are using to push privacy violation results to,
 # since there is no need to analyse this dataset for privacy violations
-datasetsToExclude = ['<dataset_name>']
+datasetsToExclude = ['spice_rdfjobs2']
 
 headers = {
         'Content-Type': 'application/json'
     }
 
 # This query should be generated with an appropriate timestamp filter (not 10)
-query = '{"$and": [ { "_timestamp": {"$gte": 10} }, { "$or" : [{"@type": "al:Update" }, {"@type": "al:Create" }] } ]}'
+query = '{"$and": [ { "_timestamp": {"$gte": '+initialTimestamp+'} }, { "$or" : [{"@type": "al:Update" }, {"@type": "al:Create" }] } ]}'
 queryParam = 'query=' + query
 pageParam = 'page=' + str(page)
 pagesizeParam = 'pagesize=' + str(pagesize)
@@ -28,9 +29,9 @@ response = requests.request("GET", alFullUrl, headers=headers, auth=HTTPBasicAut
 # Turn the JSON HTTP response into a Python-readable object
 responseObject = response.json()
 # Some attributes from the response object
-numResults = responseObject['documentCount']
-resultsPage = responseObject['page']
-resultsPageSize = responseObject['pagesize']
+numResults = int(responseObject['documentCount'])
+resultsPage = int(responseObject['page'])
+resultsPageSize = int(responseObject['pagesize'])
 
 print(numResults, resultsPage, resultsPageSize)
 if (numResults == resultsPageSize):
@@ -40,7 +41,16 @@ if (numResults == resultsPageSize):
 alEntries = responseObject['results']
 for alEntry in alEntries:
     # Exclude AL entries for datasets we have decided to exclude
+    #print(alEntry)
+    # print('Dataset: ', alEntry['al:datasetId'])
+    # print('Document ID: ', alEntry['al:documentId'])
+    # payload = alEntry['al:request']['al:payload']
+    # payloadObject = json.loads(payload)
+    # print(payloadObject)
+    # print("*****************")
+
     if alEntry['al:datasetId'] not in datasetsToExclude:
+        print('*********************')
         print('Dataset: ', alEntry['al:datasetId'])
         print('Document ID: ', alEntry['al:documentId'])
         print('Timestamp: ', alEntry['_timestamp'])
@@ -50,5 +60,6 @@ for alEntry in alEntries:
         # print each key:value pair from the payload - these values are presumably the items you will analyse for privacy violations
         for key in payloadObject:
             print(key, ":", payloadObject[key])
+
 
 
