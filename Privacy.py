d78f21f2c844223e76eaf6db0050a54468926593
Chukwudi Festus <cfu.mdx@gmail.com>
2023-02-09 08:48:57 +0000

    Updaing privacy and scanner classes

--- Privacy.py
+++ Privacy.py
@@ -1,20 +1,79 @@
-from commonregex import CommonRegex
-import spacy
-import re
+from Scanner import Scanner
+import json
+import requests
 import time
-import re
 import spacy
 import datetime
 
 
+class Privacy(Scanner):
+    # returns an array of objects for entry into the log, or an empty array
 
-class Privacy:
+    # returns an array of objects for entry into the log, or an empty array
+    def scanObject(self, datasetID, documentID, docObject):
+        items = []
+        # Initialize dictionary to store PII
+        pii = {}
+        # Loop through all the fields in the document
+        nlp = spacy.load("en_core_web_sm")
 
-    def privacyViolations(self,documentID,items):
-         total_alert_score = 0
-         severityScores = ''
+        flatObject = super().flattenObject(docObject)
 
-         for item in items:
+        for key in flatObject:
+
+            # Load spaCy model
+            # Just a basic test here, to be replaced with appropriate document scanning
+
+            entity_to_check = ['NAME', 'ADDRESS', 'CITY', 'STATE', 'PERSON', 'ORG',
+                               'COUNTRY', 'ZIP', 'PHONE', 'EMAIL', 'AGE',
+                               'STREETADDRESS', 'POSTCODE', 'GPE', 'DATE', 'IPS']
+
+            if key.upper() in entity_to_check:
+
+                doc = nlp(str(flatObject[key]))
+                for ent in doc.ents:
+                    if ent.label_ in ["PERSON", "ORG", "GPE", "LOC"]:
+                        pii[ent.label_] = ent.text
+
+                piiResult = str(pii)
+
+                extreme = 4
+                high = 3
+                medium = 2
+                low = 1
+                ascore = 0
+                ascoreName = ''
+                if piiResult in ["PERSON"]:
+                    ascore = extreme
+                    ascoreName = 'extreme'
+                elif piiResult in ["ORG"]:
+                    ascore = medium
+                    ascoreName = 'medium'
+                elif key.upper() in ["GPE", "LOC"]:
+                    ascore = low
+                    ascoreName = 'low'
+                elif key.upper() in ["NAME", "CREDITCARD", "PHONE", "EMAIL"]:
+                    ascore = extreme
+                    ascoreName = 'extreme'
+                elif key.upper() in ["ADDRESS", "STREETADDRESS"]:
+                    ascore = high
+                    ascoreName = 'high'
+                elif key.upper() in ["CITY", "STATE", "COUNTRY", "DATE"]:
+                    ascore = low
+                    ascoreName = 'low'
+                elif key.upper() in ["ZIP", "POSTCODE", "IPS", "AGE"]:
+                    ascore = medium
+                    ascoreName = 'medium'
+                else:
+                    ascore = ascore
+
+                items.append(self.__buildNotification(
+                    key, flatObject[key], ascore, ascoreName))
+
+        total_alert_score = 0
+        severityScores = ''
+
+        for item in items:
             total_alert_score += item['Alert Score']
             if total_alert_score >= 4:
                 severityScores = "extreme"
@@ -24,17 +83,30 @@ class Privacy:
                 severityScores = "medium"
             else:
                 severityScores = "low"
-      
-            return [{
-                        "job-type": "PRIVACY-VIOLATION",
-                        "Date": int(time.time()),
-                        "Document ID": documentID,
-                        "Status": "ALERT",
-                        "SeverityScores": severityScores,
-                        "Description": 'Personal identifiable information was decteted in this document',
-                        'Fields': items
-                }]
-        
 
+      # Get the current date
+        current_date = datetime.datetime.now().date()
 
-    
+        return [{
+            "job-type": "PRIVACY-VIOLATION",
+            "Date": current_date.strftime("%m/%d/%Y"),
+            "TimeStam": int(time.time()),
+            "Document ID": documentID,
+            "Status": "ALERT",
+            "SeverityScores": severityScores,
+            "Description": 'Personal identifiable information was decteted in this document',
+            'Fields': items
+        }]
+
+    def __buildNotification(self, fieldName, fieldValue, alertScore, ascoreName):
+
+        notification = {
+            "Field Name": fieldName,
+            "Value": fieldValue,
+            "PII Type": fieldName,
+            "PII Descriptiom": fieldName+' was decteted',
+            "Alert Score": alertScore,
+            "Alert Name": ascoreName,
+        }
+
+        return notification
--- Scanner.py
+++ Scanner.py
@@ -1,114 +1,34 @@
 import json
 import time
-from Privacy import Privacy
-from os import path
-import re
-import spacy
-import datetime
 
-file_path = path.abspath(__file__)
-dir_path = path.dirname(file_path)
-json_file_path = path.join(dir_path,"scanner_config.json") 
 
 class Scanner:
-    def __init__(self):
+    def __init__(self, configFilename=''):
         # Initialisation code here...
-        with open(json_file_path) as configFile:
-            self.config = json.load(configFile)
-            
-            
-        # returns an array of objects for entry into the log, or an empty array
+        if configFilename != '':
+            with open(configFilename) as configFile:
+                self.config = json.load(configFile)
+        else:
+            self.config = {}
+
+    # returns an array of objects for entry into the log, or an empty array
     def scanObject(self, datasetID, documentID, docObject):
         itemsFound = False
         items = []
-        # Initialize dictionary to store PII
-        pii = {}
         # Loop through all the fields in the document
-        nlp = spacy.load("en_core_web_sm")
-
-        flatDoc = docObject 
-        
-        self.flattenObject(docObject)
+        print('*** ' + datasetID + ':' + documentID + ' ***')
+        flatDoc = self.__flattenObject(docObject)
         for key in flatDoc:
-
-
-            # Load spaCy model
             # Just a basic test here, to be replaced with appropriate document scanning
-
-            entity_to_check = ['NAME', 'ADDRESS', 'CITY', 'STATE','PERSON','ORG',
-                            'COUNTRY', 'ZIP', 'PHONE', 'EMAIL', 'AGE', 
-                            'STREETADDRESS', 'POSTCODE','GPE','DATE','IPS']
-
-            if key.upper() in entity_to_check:
-               
-                doc = nlp(flatDoc[key])
-                for ent in doc.ents:
-                    if ent.label_ in ["PERSON", "ORG", "GPE", "LOC"]:
-                        pii[ent.label_] = ent.text
-                
-                piiResult = str(pii) 
-
-                extreme = 4
-                high = 3
-                medium = 2
-                low = 1
-                ascore = 0
-                ascoreName = ''
-                if piiResult  in ["PERSON"]:
-                    ascore = extreme
-                    ascoreName = 'extreme'
-                elif piiResult in ["ORG"]:
-                    ascore = medium
-                    ascoreName = 'medium'
-                elif key.upper() in ["GPE","LOC"]:
-                    ascore = low
-                    ascoreName = 'low'
-                elif key.upper()  in ["NAME","CREDITCARD", "PHONE", "EMAIL"]:
-                    ascore = extreme
-                    ascoreName = 'extreme'
-                elif key.upper() in ["ADDRESS", "STREETADDRESS"]:
-                    ascore = high
-                    ascoreName = 'high'
-                elif key.upper() in ["CITY", "STATE", "COUNTRY", "DATE"]:
-                    ascore = low
-                    ascoreName = 'low'
-                elif key.upper() in ["ZIP", "POSTCODE", "IPS","AGE"]:
-                    ascore = medium
-                    ascoreName = 'medium'
-                else:
-                    ascore = ascore
-                
-                items.append(self.__buildNotification(key, flatDoc[key],ascore,ascoreName))
-
-        total_alert_score = 0
-        severityScores = ''
-
-        for item in items:
-            total_alert_score += item['Alert Score']
-            if total_alert_score >= 4:
-                severityScores = "extreme"
-            elif total_alert_score == 3:
-                severityScores = "high"
-            elif total_alert_score == 2:
-                severityScores = "medium"
-            else:
-                severityScores = "low"
-      
-      # Get the current date
-        current_date = datetime.datetime.now().date()
-
-        return [{
-                    "job-type": "PRIVACY-VIOLATION",
-                    "Date": current_date.strftime("%m/%d/%Y"),
-                    "TimeStam": int(time.time()),
-                    "Document ID": documentID,
-                    "Status": "ALERT",
-                    "SeverityScores": severityScores,
-                    "Description": 'Personal identifiable information was decteted in this document',
-                    'Fields': items
-            }]
-
-
+            print(key + ':' + str(flatDoc[key]))
+            '''
+            if '@' in docObject[key]:
+                # build an appropriate notification
+                itemsFound = True
+                message = 'An @ symbol was found, this may indicate a email address'
+                items.append(self.__buildNotification(datasetID, documentID, key, docObject[key], message))
+            '''
+        return items
 
     def flattenObject(self, originalDocument):
         out = {}
@@ -128,16 +48,16 @@ class Scanner:
         flatten(originalDocument)
         return out
 
-
-    def __buildNotification(self,fieldName, fieldValue,alertScore,ascoreName):
-
+    def __buildNotification(self, datasetID, documentID, fieldName, fieldValue, message):
         notification = {
-            "Field Name": fieldName,
-            "Value": fieldValue,
-            "PII Type": fieldName,
-            "PII Descriptiom": fieldName+' was decteted',
-            "Alert Score": alertScore,
-            "Alert Name": ascoreName,
+            "job-type": "SAMPLE-NOTIFICATION",
+            "submitted-by": "LDH-SCANNER",
+            "modified": int(time.time()),
+            "message": message,
+            "dataset": datasetID,
+            "document": documentID,
+            "field": fieldName,
+            "value": fieldValue,
+            "status": "ALERT"
         }
-       
         return notification
